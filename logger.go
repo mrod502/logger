81ad40554363f87dca66331a9291636f904a11f7
Mike <mrod22502@gmail.com>
2020-10-27 13:56:58 -0400

    init

--- logger.go
+++ logger.go
@@ -1,1 +1,21 @@
 package logger
+
+var logChan chan string
+
+func init(){
+	logChan=make(chan string,128)
+	go func(){
+		for{
+			fmt.Println(<-logChan)
+		}
+	}
+}
+
+
+func Log(in []string){
+	msg:=""
+	for _,v:=range in{
+		msg+=v+" "
+	}
+	logChan<-msg
+}
